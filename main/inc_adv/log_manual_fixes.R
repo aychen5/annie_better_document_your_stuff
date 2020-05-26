2784a6acefb4e7f4371390c13c84e465d10884bc
Annie Chen <aychen.555@gmail.com>
2020-05-26 13:05:35 -0400

    minor refinement to regex for names

--- main/inc_adv/log_manual_fixes.R
+++ main/inc_adv/log_manual_fixes.R
@@ -71,4 +71,138 @@ clean_fp_df <- clean_fp_df %>%
   bind_rows(mallee_df) %>% 
   arrange(division_name)
 
+} else if (state == "qld" & year == 1998) {
+
+### --------------- 1998 QLD --------------- ### 
+
+# redisticting 
+
+# Herbert division: there are two candidates named Lindsay (last name)
+clean_fp_df[clean_fp_df$candidate_name == "Hon Ted Lindsay", "last_name"] <- "T.LINDSAY"
+clean_fp_df[clean_fp_df$candidate_name == "Peter Lindsay", "last_name"] <- "P.LINDSAY"
+clean_tcp_df[clean_tcp_df$last_name == "T", "last_name"] <- "T.LINDSAY"
+clean_tcp_df[clean_tcp_df$last_name == "P", "last_name"] <- "P.LINDSAY"
+
+clean_tcp_df[clean_tcp_df$last_name == "M", "last_name"] <- "SMITH"
+
+} else if (state == "wa" & year == 1998) {
+  
+  ### --------------- 1998 WA --------------- ### 
+  #Hon Warwick Smith
+  clean_tcp_df[clean_tcp_df$last_name == "W", "last_name"] <- "SMITH"
+  
+} else if (state == "nsw|qld|vic" & year == 1996) {
+  
+  # remove all lines with "exhausted" 
+  detect_anomaly <- list()
+  for (i in 1:length(lines[[1]])){
+    detect_anomaly[[i]] <- ifelse(str_detect(lines[[1]][i], "exhausted"), 1, 0)
+  }
+  
+  out <- data.frame(ind = 1:length( lines[[1]]),
+             exhaust = unlist(detect_anomaly),
+             text = lines[[1]]) %>% 
+    filter(exhaust == 0 ) %>% 
+    select(text) 
+  lines[[1]] <- as.character(out$text)
+  
+  ## Cunningham is misscoded in the text file (previous election, 1990 should be 1993)
+
+} else if (state == "qld" & year == 1996) {
+
+    # new seat## MALLEE is a new division in the text file?
+  longman_df <- data.frame(
+    division_name = "LONGMAN",
+    candidate_name = c(
+      "Jim Dimo",         
+      "Pat Bonnice",                       
+      "Terence Madden",                         
+      "Norman Hegarty",                   
+      "Mal Brough",               
+      "Tom Bradley",
+      "Greg Hollis",
+      "Geoffrey Abnett"),                  
+    incumbent = c(0, 0, 0, 0, 0, 0, 0, 0),
+    candidate_party = c("Grn", "ALP", NA, NA, "Lib", "NPA", "AD", NA),
+    vote_count = as.character(c(2145, 21943, 966, 187, 27704, 12611, 5276, 673)),
+    fp_vote_share = c(3.0, 30.7, 1.4, 0.3, 39.7, 17.6, 7.4, 0.9),
+    last_name = toupper(c("Dimo", 
+                          "Bonnice", 
+                          "Madden", 
+                          "Hegarty", 
+                          "Brough", 
+                          "Bradley", 
+                          "Hollis",
+                          "Abnett"))
+  )
+  
+  # attach to main df
+  clean_fp_df <- clean_fp_df %>% 
+    bind_rows(longman_df) %>% 
+    arrange(division_name)
+  
+} else if (state == "tas" & year == 1996) {
+  clean_tcp_df[clean_tcp_df$last_name == "S", "last_name"] <- "SMITH"
+  clean_tcp_df[clean_tcp_df$last_name == "W", "last_name"] <- "SMITH"
+} else if (state == "nsw" & year == 1993) {
+  
+  clean_tcp_df[clean_tcp_df$last_name == "B", "last_name"] <- "WOODS"
+  
+} else if (state == "vic" & year == 1993) {
+  clean_tcp_df[clean_tcp_df$division_name == "MELBOURNE", "candidate_name"] <- c("Lindsay TANNER", "Riza Kozanoglu")
+  clean_tcp_df[clean_tcp_df$division_name == "MELBOURNE", "last_name"] <- c("TANNER", "KOZANOGLU")
+  clean_tcp_df[clean_tcp_df$division_name == "MELBOURNE", "tcp_vote_share"] <- c(73.7, 26.3)
+  
+} else if (state == "tas" & year == 1993) {
+
+
+clean_tcp_df[clean_tcp_df$last_name == "S", "last_name"] <- "SMITH"
+clean_tcp_df[clean_tcp_df$last_name == "W", "last_name"] <- "SMITH"
+
+
+}  else if (state == "act" & year == 1993) {
+
+
+clean_tcp_df[clean_tcp_df$last_name == "R", "last_name"] <- "KELLY"
+
+}  else if (state == "nt" & year == 1993) {
+  
+  
+  clean_tcp_df[clean_tcp_df$last_name == "ARTHUR", "last_name"] <- "PALMER"
+  clean_tcp_df[clean_tcp_df$last_name == "HON", "last_name"] <- "SNOWDON"
+  
+} else if (state == "nsw" & year == 1990) {
+  
+  
+  clean_fp_df[clean_fp_df$candidate_name == "David de", "last_name"] <- "MONTFORT"
+  clean_fp_df[clean_fp_df$candidate_name == "George der", "last_name"] <- "MATTOSIAN"
+}else if (state == "wa" & year == 1990) {
+  
+  
+  clean_fp_df[clean_fp_df$division_name == "O", "division_name"] <- as.factor("O'CONNOR")
+} else if (state == "nsw" & year == 1987) {
+  
+  clean_tcp_df[clean_tcp_df$candidate_name == "Warren Musgrave", "last_name"] <- "MUSGRAVE"
+  clean_tcp_df[clean_tcp_df$candidate_name == "David Brock", "last_name"] <- "BROCK"
+  clean_tcp_df[clean_tcp_df$candidate_name == "Russell GORMAN *", "last_name"] <- "GORMAN"
+  clean_tcp_df[clean_tcp_df$candidate_name == "Hon John KERIN *", "last_name"] <- "KERIN"
+  clean_tcp_df[clean_tcp_df$candidate_name == "Peter Black", "last_name"] <- "BLACK"
+  clean_tcp_df[clean_tcp_df$candidate_name == "Geoff Robinson", "last_name"] <- "ROBINSON"
+  clean_tcp_df[clean_tcp_df$candidate_name == "Noel HICKS *", "last_name"] <- "HICKS"
+  clean_tcp_df[clean_tcp_df$candidate_name == "Alan CADMAN *", "last_name"] <- "CADMAN"
+} else if (state == "vic" & year == 1987) {
+
+clean_tcp_df[clean_tcp_df$candidate_name == "ST-APLES *", "last_name"] <- "STAPLES"
+} else if (state == "tas" & year == 1987) {
+
+clean_tcp_df[clean_tcp_df$candidate_name == "Christopher MILES *", "last_name"] <- "MILES"
+clean_tcp_df[clean_tcp_df$candidate_name == "David Currie", "last_name"] <- "CURRIE"
+
+
 }
+
+
+
+
+
+
--- main/inc_adv/parse_australia_txt.R
+++ main/inc_adv/parse_australia_txt.R
@@ -23,23 +23,39 @@ read_file_fxn <- function (year, state) {
   # directory containing files
   path <- "~/Dropbox/Thesis/inc_adv/raw_data/federal/lower"
   year_dir <- paste0("/", year)
-  file_name <- paste0("/fp_", state, "_", year, ".txt")
+  file_name <- paste0("/fp_", tolower(state), "_", year, ".txt")
   
   data <- readtext(paste0(path, year_dir, file_name)) 
   # separate lines by \n
   lines <- str_split(data$text, pattern = "\n")
   
+  # remove all lines with "exhausted"
+  detect_anomaly <- list()
+  for (i in 1:length(lines[[1]])){
+    detect_anomaly[[i]] <- ifelse(str_detect(lines[[1]][i], "exhausted"), 1, 0)
+  }
+  
+  out <- data.frame(ind = 1:length( lines[[1]]),
+                    exhaust = unlist(detect_anomaly),
+                    text = lines[[1]]) %>% 
+    filter(exhaust == 0 ) %>% 
+    select(text) 
+  lines[[1]] <- as.character(out$text)
+  
   return(lines)
 } 
 
 #list.files(path, pattern = ".txt")
-states <- c("nsw", "vic", "qld", "wa", "sa", "tas", "nt", "act")
-elect_year <- 1998
+#states <- c("nsw", "vic", "qld", "wa", "sa", "tas", "nt", "act")
+elect_year <- 1987
+prev_year <- "1984"
+state <- "ACT"
 
 # read in all states in a year
-map(states, function (x) read_file_fxn(year = elect_year, state = x))
+#map(states, function (x) read_file_fxn(year = elect_year, state = x))
 
-lines <- read_file_fxn(year = elect_year, state = "vic")
+lines <- read_file_fxn(year = elect_year, state = state)
+
 
 ########## How is the raw data formatted? ##########
 # Each text file are the results for one State, 
@@ -56,7 +72,7 @@ lines <- read_file_fxn(year = elect_year, state = "vic")
 ########### ------------------------ FP ------------------------ #################
 
 
-parse_fp_fxn <- function (previous_election, state) {
+parse_fp_fxn <- function (previous_election, state, elect_year) {
   # previous_election: a string, year of previous election
   # state: a string, one of the Australian states (NSW, QLD, WA, ...)
   # fxn returns a dataframe of fp distributions for each division
@@ -73,20 +89,23 @@ parse_fp_fxn <- function (previous_election, state) {
 # loop through all lines and get the relevant ones
 for (i in 1:length(lines[[1]])){
   # division names (takes hyphens too)
-  if(str_detect(lines[[1]][i], paste0("[A-Z]+(\\-[A-Z]+)?, ", state))) {
-    division_names[i] <- str_extract(string = lines[[1]][i], pattern = "[A-Z]+((\\-|\\s)[A-Z]+)?")
+  if(str_detect(lines[[1]][i], paste0("[A-Z]+((c|\\-|\\s|\\')?[A-Z]+)?, ", state))) {
+    division_names[i] <- str_extract(string = lines[[1]][i], pattern = "[A-Z]+((c|\\-|\\s|\\')?[A-Z]+)?")
     divisionid[i] <- id_count + 1
     id_count <- id_count + 1
   } else  {division_names[i] <- NA}
   
   #first preferences 
-    if(str_detect(lines[[1]][i], paste0(previous_election, " two-party majority:"))) { # this string is previous election
+    if(str_detect(lines[[1]][i], paste0(previous_election, " two-party majority:", "|",
+                                        previous_election, " notional two-party majority:", "|",
+                                        "New seat"))) { # this string is previous election
       
-      # there are divisions that also had by-elections, so have to start at line one later
-      start <- if_else(str_detect(lines[[1]][i + 1], "by-election"), 
-                       i + 2,
-                       #  2 lines below begins candidates, first preferences
-                       i + 1)
+      # there are divisions that also had by-elections/redistricting, so have to start at line one later
+      if(str_detect(lines[[1]][i + 2], "New notional")){
+        start <- i + 3 #  3 lines below begins candidates, first preferences
+      } else if (str_detect(lines[[1]][i + 1], "by-election|redistribution")) {
+        start <- i + 2 #  2 lines below begins candidates, first preferences
+      } else {start <- i + 1 }
       
       # the number of candidates varies between divisions,
       # print all names between the dashes
@@ -103,21 +122,15 @@ for (i in 1:length(lines[[1]])){
     division_name = as.character(lag(division_names, n = 6L)),
     division_id = as.numeric(lag(divisionid, n = 6L)))
   
-  # Some divisions are not lagged by 6 rows
-  for (i in 1:nrow(fp_df)) {
-    if((!is.na(fp_df$division_name[i])) & is.na(fp_df$fp[i]) & (!is.na(fp_df$fp[i+1]))) {
-      fp_df$division_name[i+1] <- fp_df$division_name[i]
-    } else {NA} 
-  }
-  
   # dataframe of first preferences
   clean_fp_df <- fp_df %>% 
-    filter(!is.na(fp)) %>% 
     # interpolate the divisions
-    zoo::na.locf(maxgap = 20, na.rm = FALSE) %>% 
+    mutate(division_name = zoo::na.locf(division_name, na.rm = FALSE)) %>% 
+    # remove extra rows
+    filter(!is.na(fp)) %>% 
     # add incumbent
     mutate(incumbent = if_else(str_detect(fp, pattern = "\\*"), 1, 0),
-           year = 1998,
+           year = elect_year,
            state = toupper(state)) %>% 
     # parse party and fp vote share
     separate(fp, into = c("candidate_name", "other"), sep = "   ", extra = "merge") %>% 
@@ -132,29 +145,55 @@ for (i in 1:length(lines[[1]])){
            candidate_name = if_else(str_detect(candidate_name, "St\\s[A-Za-z]+"), 
                                     gsub("St\\s", "ST-", candidate_name), candidate_name)) %>% 
     #remove asterisks first; also tricky b/c some have "Hon" preceding name (make optional), can be "Hon Dr"
-    mutate(candidate_name = str_extract(candidate_name, "((Hon|Dr)\\s)?(Dr\\s)?[A-Za-z]+\\s+[A-Za-z]+((\\'|\\-)[A-Za-z]+)?")) %>% 
+    mutate(candidate_name = str_extract(candidate_name, "((Rt|Hon|Dr)\\s)?((Hon|Dr)\\s)?[A-Za-z]+\\s+[A-Za-z]+(?:[\\'|\\-][A-Za-z]+)?")) %>% 
     #first extract last names, and make all uppercase
-    mutate(last_name = toupper(str_extract(candidate_name, "[A-Za-z]+(?:[\\'\\-][a-zA-Z]+)*$"))) 
+    mutate(last_name = toupper(str_extract(candidate_name, "[A-Za-z]+(?:[\\'|\\-][a-zA-Z]+)*$"))) 
   
+  # # NT has no divisions
+  if(state == "NT") {
+    clean_fp_df <- clean_fp_df %>% mutate(division_name = rep("NORTHERN TERRITORY", nrow(clean_fp_df)))
+  }
+  # 
   return(clean_fp_df)
 }
 
 #source(here::here("parse_fp_fxn."))
 
-clean_fp_df <- parse_fp_fxn(previous_election = "1996", state = "Vic")
-#View(clean_fp_df)
+clean_fp_df <- parse_fp_fxn(previous_election = prev_year, 
+                            state = state, 
+                            elect_year = elect_year)
+View(clean_fp_df)
 
 ########### ------------------------ TCP ------------------------ #################
 
-parse_tcp_fxn <- function (state) {
- 
+parse_tcp_fxn <- function (state, elect_year) {
+  
+  #the final count
+  tcp <- list("character")
+  
+  # there's only one div for NT
+  if(state == "NT") {
+    detect_lines <- c()
+    last_lines <- tail(lines[[1]], n = 15)
+    
+    for (k in 1:length(last_lines)) {  
+      # same trick as before
+      detect_lines[k] <- str_detect(last_lines[16-k], "---")
+      line_separator_id <- (length(last_lines)+1) - which(detect_lines)[3]
+    }
+    
+    # add last division to rest of list
+    tcp <- c("NORTHERN TERRITORY",
+             last_lines[line_separator_id-1],
+             last_lines[line_separator_id-2])
+    
+    tcp_df <- data.frame(tcp = tcp)
+  } else {
+  
   ### get divisions with extra comments section with custom function ###
   source(here::here("fourline_divs_fxn.R"))
   fourth_line_divs <- fourline_divs_fxn(state = state)
   # state: a string, one of the Australian states (NSW, QLD, WA, ...)
-  
-  #the final count
-  tcp <- list("character")
   
   for (i in 1:length(lines[[1]])){
     ### final count (tcp) ###
@@ -165,7 +204,7 @@ parse_tcp_fxn <- function (state) {
                                     data = lines[[1]],
                                     index = i) {
         detect_lines <- c()
-        for (j in 1:15) {
+        for (j in 1:16) {
           detect_lines[j] <- str_detect(data[index-j], "---")
         }
         # the two-party count is (often) above the third "---" separator,
@@ -182,8 +221,8 @@ parse_tcp_fxn <- function (state) {
       ### an extra section of comments
       fourth_line <- paste0(fourth_line_divs, 
                             collapse = "|")
-      
-      if (str_detect(lines[[1]][i], fourth_line)) {
+      # if there are any fourth line separators, skip an extra line
+      if (str_detect(lines[[1]][i], fourth_line) & fourth_line != "") {
         tcp[[i]] <- detect_lines_fxn(separator_number = 4) 
       } else {
         tcp[[i]] <- detect_lines_fxn(separator_number = 3) 
@@ -194,12 +233,12 @@ parse_tcp_fxn <- function (state) {
   
   ### don't forget to add the last division ###
   detect_lines <- c()
-  last_lines <- tail(lines[[1]], n = 10)
+  last_lines <- tail(lines[[1]], n = 15)
   
-  for (k in 1:(length(last_lines)-1)) {  
+  for (k in 1:length(last_lines)) {  
     # same trick as before
-    detect_lines[k] <- str_detect(last_lines[10-k], "---")
-    line_separator_id <- 10-which(detect_lines)[3]
+    detect_lines[k] <- str_detect(last_lines[16-k], "---")
+    line_separator_id <- (length(last_lines)+1) - which(detect_lines)[3]
   }
   
   # add last division to rest of list
@@ -210,7 +249,7 @@ parse_tcp_fxn <- function (state) {
   ### need to align the divisions 
   tcp_df <- data.frame(tcp = tcp) %>% 
     filter(!str_detect(tcp, "character"))
-  
+  }
   
   divs_df <- tcp_df %>% 
     # all the divisions
@@ -230,21 +269,23 @@ parse_tcp_fxn <- function (state) {
              c(1, seq(from = 2, to = nrow(tcp_df), by = 3)[-max(length(seq(from = 2, to = nrow(tcp_df), by = 3)))])) %>% 
     bind_cols(divs_df) %>% 
     mutate(state = toupper(state), 
-           year = 1998) %>% 
+           year = elect_year) %>% 
     separate(tcp, into = c("candidate_name", "other"), sep = "\\s{2}", extra = "merge") %>% 
     #(there are some encoding issues with apostrophes and replace "ST" with "ST-")
     mutate(candidate_name = if_else(str_detect(candidate_name, "[\u0092\u2018\u2019\u201A\u201B\u2032\u2035]"), 
                                     gsub("[\u0092\u2018\u2019\u201A\u201B\u2032\u2035]", "'", candidate_name), candidate_name),
-           candidate_name = if_else(str_detect(candidate_name, "^st|^ST\\s"), 
-                                    gsub("^st|^ST\\s", "ST-", candidate_name), candidate_name)) %>% 
+           candidate_name = if_else(str_detect(candidate_name, "^st|^ST|^St\\s"), 
+                                    gsub("^st|^ST|^St\\s", "ST-", candidate_name), candidate_name)) %>% 
     #remove asterisks, may have apostrophe or hyphen in name
-    mutate(last_name = toupper(str_extract(candidate_name, "[A-Za-z]+(?:['-][a-zA-Z]+)*")),
+    mutate(last_name = toupper(str_extract(candidate_name, "[A-Za-z]+((\\'|\\-)?[a-zA-Z]+)*")),
            tcp_vote_share = str_extract(other, "\\s+[0-9]+\\.[0-9]"))
   
   return(clean_tcp_df)
 }
 
-clean_tcp_df <- parse_tcp_fxn(state = "Vic")
+clean_tcp_df <- parse_tcp_fxn(state = state,
+                              elect_year = elect_year)
+View(clean_tcp_df)
 
 #--------------------- CHECKS --------------------- #
 # match candidate names in clean_fp_df to clean_tcp_df to figure out the party
@@ -257,18 +298,18 @@ tryCatch(
   finally = print(setdiff(clean_tcp_df$last_name, clean_fp_df$last_name))
          )
 
+View(clean_tcp_df)
+View(clean_fp_df) 
+
 #--------------------- MANUAL FIXES/ANOMALIES --------------------- #
 
 # see log_manual_fixes.R script to see all the fixes 
 # that are unique to the text file.
-state <- "vic"
-year <- elect_year
-source(here::here("log_manual_fixes.R"))
+#source(here::here("log_manual_fixes.R"))
 
 #--------------------- SAVE CLEAN TCP DATA --------------------- #
 
-View(clean_tcp_df)  
-View(clean_fp_df) 
+
 
 # add parties to candidates in tcp data
 clean_tcp_df <- clean_tcp_df %>% 
@@ -276,6 +317,7 @@ clean_tcp_df <- clean_tcp_df %>%
   full_join(clean_fp_df, by = c("division_name", "state", "year", "last_name")) %>% 
   filter(!is.na(tcp_vote_share)) %>% 
   select(-c(other, candidate_name.x, swing, division_id))
+
 
 save_file_fxn <- function(year, state) {
   
@@ -289,6 +331,6 @@ save_file_fxn <- function(year, state) {
   
 }
 
-save_file_fxn(year = 1998, state = "vic")
+save_file_fxn(year = elect_year, state = tolower(state))
 
 
